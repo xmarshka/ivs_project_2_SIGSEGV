73a6ff43375246c34a158c4cbe496081c2fdb9af
Andrej Smatana <andrejsmatana0@gmail.com>
2022-04-28 19:44:55 +0200

    Update README.md

---
+++ src/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Kalkulacka',
+    version='1.0',
+    packages=['Kalkulacka'],
+    url='',
+    license='',
+    author='',
+    author_email='',
+    description=''
+)
